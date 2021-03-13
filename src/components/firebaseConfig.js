beac4df0fd0d5a796c7d8bf346a419cfb4fd083c
alperen <64660609+alperenkarate@users.noreply.github.com>
2021-03-13 21:40:12 +0300

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyBw-z11nWVxNXG6QnG0NTcoMudqRJbxGWc",
+    authDomain: "vuecrud-84cef.firebaseapp.com",
+    projectId: "vuecrud-84cef",
+    storageBucket: "vuecrud-84cef.appspot.com",
+    messagingSenderId: "1014495643737",
+    appId: "1:1014495643737:web:f43ac09662996c872f4948"
+}
