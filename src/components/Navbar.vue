a78536948645b57a8705791d357c80bcc4c7091e
alperen <64660609+alperenkarate@users.noreply.github.com>
2021-03-16 22:29:06 +0300

    routing active styles are added.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,20 @@
 <template>
-   <nav>
-       <div class="nav-wrapper green">
-           <div class="container">
-               <router-link to="/" class="brand-logo">
-               Employee Manager
-               </router-link>
-           </div>
-       </div>
-   </nav>
+  <nav>
+    <div class="nav-wrapper green">
+      <div class="container">
+        <router-link to="/"> Employee Manager </router-link>
+        <router-link to="/new"> 2.route </router-link>
+      </div>
+    </div>
+  </nav>
 </template>
+
+
+
+
+<style>
+.router-link-exact-active {
+  color: black;
+  font-weight: 800;
+}
+</style>
